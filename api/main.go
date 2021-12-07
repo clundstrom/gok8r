c49964217f54d3ae181de17261ede3cf32a39de6
Christoffer Lundström <lundstromc90@gmail.com>
2021-12-07 21:55:50 +0100

    Modified templates and build script

--- api/main.go
+++ api/main.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 )
 
-// Get preferred outbound ip of this machine
 func GetOutboundIP() net.IP {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
--- build.sh
+++ build.sh
@@ -20,10 +20,34 @@ frontend(){
     fi
 }
 
+broker(){
+    echo "Building event broker"
+    docker build ./rabbitmq -t rabbitmq
+
+    if [[ $1 == '-r' ]]; then
+    echo "Starting frontend in detached mode.."
+    docker run --name rabbitmq -d -t -p "5672:5672" rabbitmq
+    fi
+}
+
+package(){
+  echo "Packaging helm charts"
+  DIR=$PWD
+  cd "$PWD/gok8r/packages"
+  helm package ../.
+  cd "$DIR"
+}
+
+deploy(){
+  helm install gok8r --generate-name
+}
+
+
 
 if [[ $# -eq 0 ]]; then
   api "-r"
   frontend "-r"
+  broker "-r"
   exit 0
 fi
 
@@ -41,9 +65,15 @@ for i in "$@"; do
     "frontend") 
       frontend $run
       ;;
+    "package") 
+    package
+    ;;
+    "deploy") 
+    deploy
+    ;;
     *)
       echo 'One or more invalid arguments.'
-      echo "Usage: $0 [OPTIONAL] {db|frontend|{empty}}"
+      echo "Usage: $0 [OPTIONAL] {db|frontend|broker|charts|{empty}}"
       echo 'Optional: < -r > - Build and run container >'
       echo "Example: $0 -r api frontend"
       exit 1
